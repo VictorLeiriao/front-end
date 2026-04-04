e617e770a6f2209cf9e5a8e7aeda1d5d2ced3b8a
Victor Leirião <victor.augusto.1907@icloud.com>
2026-04-04 15:56:21 -0300

    ...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,32 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
+import '../styles/globals.css'; 
+import '@rainbow-me/rainbowkit/styles.css'; 
 
+import type { AppProps } from 'next/app';
+import Head from 'next/head'; // <-- NOSSA ARMA SECRETA
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 
-import { config } from '../wagmi';
+import { config } from '../wagmi'; 
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <>
+      {/* INJETAMOS O TAILWIND DIRETO NA VEIA DO SITE AQUI! */}
+      <Head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={client}>
+          <RainbowKitProvider theme={darkTheme()}>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
   );
 }
 

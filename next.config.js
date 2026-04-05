cf4aca737cf2db95824b77190b35d4576cfab6dd
Victor Leirião <victor.augusto.1907@icloud.com>
2026-04-05 13:53:31 -0300

    ..

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <-- MÁGICA ADICIONADA AQUI
   reactStrictMode: true,
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
